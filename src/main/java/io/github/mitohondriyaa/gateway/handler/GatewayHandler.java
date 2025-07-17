2ac25a99daeb7782514adae31d356818477d2cae
Mitohondriyaa <mitohondriyaa@gmail.com>
2025-07-17 15:24:44 +0300

    Refactor fallback to return JSON

--- src/main/java/io/github/mitohondriyaa/gateway/handler/GatewayHandler.java
+++ src/main/java/io/github/mitohondriyaa/gateway/handler/GatewayHandler.java
@@ -6,10 +6,16 @@ import org.springframework.web.reactive.function.server.ServerRequest;
 import org.springframework.web.reactive.function.server.ServerResponse;
 import reactor.core.publisher.Mono;
 
+import java.util.Collections;
+import java.util.Map;
+
 @Component
 public class GatewayHandler {
+    private static final Map<String, String> fallbackBody
+        = Collections.singletonMap("info", "Service unavailable, please try again later");
+
     public Mono<ServerResponse> fallbackHandle(ServerRequest request) {
         return ServerResponse.status(HttpStatus.SERVICE_UNAVAILABLE)
-            .body(Mono.just("Service unavailable, please try again later"), String.class);
+            .body(Mono.just(fallbackBody), Map.class);
     }
 }
