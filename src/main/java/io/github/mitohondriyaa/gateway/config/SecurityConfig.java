4f3f5b2d3787195272e05c4bf75235c80b27aa35
Mitohondriyaa <mitohondriyaa@gmail.com>
2025-07-30 20:46:17 +0300

    Add authentication and authorization error handling

--- src/main/java/io/github/mitohondriyaa/gateway/config/SecurityConfig.java
+++ src/main/java/io/github/mitohondriyaa/gateway/config/SecurityConfig.java
@@ -3,11 +3,49 @@ package io.github.mitohondriyaa.gateway.config;
 import io.github.mitohondriyaa.gateway.converter.KeycloakRealmRoleConverter;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.io.buffer.DataBuffer;
+import org.springframework.http.HttpStatus;
 import org.springframework.security.config.web.server.ServerHttpSecurity;
 import org.springframework.security.web.server.SecurityWebFilterChain;
+import org.springframework.security.web.server.ServerAuthenticationEntryPoint;
+import org.springframework.security.web.server.authorization.ServerAccessDeniedHandler;
+import reactor.core.publisher.Mono;
 
 @Configuration
 public class SecurityConfig {
+    @Bean
+    public ServerAuthenticationEntryPoint authenticationEntryPoint() {
+        return (exchange, ex) -> {
+            byte[] body = """
+                {
+                    "code": "UNAUTHORIZED",
+                    "message": "User not authenticated"
+                }
+                """.getBytes();
+            DataBuffer dataBuffer = exchange.getResponse().bufferFactory().wrap(body);
+
+            exchange.getResponse().setStatusCode(HttpStatus.UNAUTHORIZED);
+
+            return exchange.getResponse().writeWith(Mono.just(dataBuffer));
+        };
+    }
+
+    @Bean
+    public ServerAccessDeniedHandler accessDeniedHandler() {
+        return (exchange, denied) -> {
+            byte[] body = """
+                {
+                    "code": "FORBIDDEN",
+                    "message": "User not authorized"
+                }
+                """.getBytes();
+            DataBuffer dataBuffer = exchange.getResponse().bufferFactory().wrap(body);
+
+            exchange.getResponse().setStatusCode(HttpStatus.FORBIDDEN);
+            return exchange.getResponse().writeWith(Mono.just(dataBuffer));
+        };
+    }
+
     @Bean
     public SecurityWebFilterChain securityFilterChain(ServerHttpSecurity http) {
         return http.authorizeExchange(exchange -> exchange
@@ -23,6 +61,8 @@ public class SecurityConfig {
                 .anyExchange()
                 .authenticated())
                 .oauth2ResourceServer(oauth2 -> oauth2
+                    .authenticationEntryPoint(authenticationEntryPoint())
+                    .accessDeniedHandler(accessDeniedHandler())
                     .jwt(jwt -> jwt.jwtAuthenticationConverter(new KeycloakRealmRoleConverter())))
                 .build();
     }
