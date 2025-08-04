639351cc99480b399d4fbe804e6d5096e43704f5
Mitohondriyaa <mitohondriyaa@gmail.com>
2025-08-04 21:15:37 +0300

    Add message if service is not available

--- src/main/java/io/github/mitohondriyaa/gateway/filter/ServicesFilter.java
+++ src/main/java/io/github/mitohondriyaa/gateway/filter/ServicesFilter.java
@@ -9,6 +9,10 @@ import io.github.resilience4j.timelimiter.TimeLimiterRegistry;
 import lombok.RequiredArgsConstructor;
 import org.springframework.cloud.gateway.filter.GatewayFilter;
 import org.springframework.cloud.gateway.filter.GatewayFilterChain;
+import org.springframework.core.io.buffer.DataBuffer;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.server.reactive.ServerHttpResponse;
 import org.springframework.stereotype.Component;
 import org.springframework.web.server.ServerWebExchange;
 import reactor.core.publisher.Mono;
@@ -30,6 +34,18 @@ public class ServicesFilter implements GatewayFilter {
 
         return chain.filter(exchange)
             .transformDeferred(TimeLimiterOperator.of(timeLimiter))
-            .transform(CircuitBreakerOperator.of(circuitBreaker));
+            .transform(CircuitBreakerOperator.of(circuitBreaker))
+            .onErrorResume(exception -> {
+                ServerHttpResponse response = exchange.getResponse();
+                response.setStatusCode(HttpStatus.SERVICE_UNAVAILABLE);
+                byte[] responseBody = """
+                    {
+                        "message": "Service unavailable, please try again later"
+                    }
+                    """.getBytes();
+                DataBuffer dataBuffer = response.bufferFactory().wrap(responseBody);
+                response.getHeaders().setContentType(MediaType.APPLICATION_JSON);
+                return response.writeWith(Mono.just(dataBuffer));
+            });
     }
 }
